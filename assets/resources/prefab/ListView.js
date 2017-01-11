23a1dbdae92ba67a22c35907e7d3e195924d687d
Robin <gejw0623@gmail.com>
2017-01-11 23:12:13 +0800

    混存

--- assets/resources/prefab/ListView.js
+++ assets/resources/prefab/ListView.js
@@ -1,18 +1,44 @@
 cc.Class({
     extends: cc.Component,
+    
+    // cell数量
+    numberOfRowsCallback: Function,
+    // cell高度
+    heightOfRowCallback: Function,
+    // cell的view
+    cellForRowAtIndexCallback: Function,
+    // cell点击
+    cellForAtIndexClickedCallback: Function,
 
     properties: {
-        numberOfRowsCallback: Function,
-        cellForRowAtIndexCallback: Function
+        
     },
 
     // use this for initialization
     onLoad: function () {
-
+        this.spawnCount = 20;
+        this.items = [];
+        this.initialize();
+        this.updateTimer = 0;
+        this.updateInterval = 0.2;
+        this.lastContentPosY = 0;
+    },
+    
+    initialize: function() {
+        this.content.height = this.totalCount * (this.itemTemplate.height + this.spacing) + this.spacing; // get total content height
+    	for (let i = 0; i < this.spawnCount; ++i) { // spawn items, we only need to do this once
+    		let item = cc.instantiate(this.itemTemplate);
+    		this.content.addChild(item);
+    		item.setPosition(0, -item.height * (0.5 + i) - this.spacing * (i + 1));
+    		item.getComponent('Item').updateItem(i, i);
+            this.items.push(item);
+    	}
     },
     
     update: function(dt) {
-        
+        this.updateTimer += dt;
+        if (this.updateTimer < this.updateInterval) return; // we don't need to do the math every frame
+        this.updateTimer = 0;
     }
 
     // called every frame, uncomment this function to activate update callback
--- assets/resources/prefab/Spinner.js
+++ assets/resources/prefab/Spinner.js
@@ -1,3 +1,5 @@
+var ListView = require("ListView");
+
 cc.Class({
     extends: cc.Component,
 
@@ -6,31 +8,29 @@ cc.Class({
         valueLabel: cc.Label,
         expandSprite: cc.Sprite,
         touchButton: cc.Button,
-        scrollView: cc.ScrollView
+        scrollView: cc.ScrollView,
+        
+        outsideButton: cc.Button
     },
 
     // use this for initialization
     onLoad: function () {
         this.expandSprite.node.active = false;
         this.touchButton.node.on("click", this.spinnerClicked, this);
-        
-        // 初始化滚动组件
-    	this.content = this.scrollView.content;
-    	this.items = [];
-    	this.updateTimer = 0;
-    	this.updateInterval = 0.2;
-    	this.lastContentPosY = 0;
-    },  
-    
-    update: function(dt) {
-        this.updateTimer += dt;
-        if (this.updateTimer < this.updateInterval) return;
-        this.updateTimer = 0;
     },
     
     spinnerClicked: function() {
         this.expandSprite.node.active = !this.expandSprite.node.active;
-    }
+        if (this.expandSprite.node.active) {
+            
+        } else {
+            this.hideOutSideButton();
+        }
+    },
+    
+    outsideButtonClicked: function() {
+        this.hideOutSideButton();
+    },
 
     // called every frame, uncomment this function to activate update callback
     // update: function (dt) {
