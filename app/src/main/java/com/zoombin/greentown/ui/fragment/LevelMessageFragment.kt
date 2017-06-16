677e15a11ea20952c922672dc5a25a23abdb13bc
Robin <gejw0623@gmail.com>
2017-06-16 17:31:14 +0800

    留言功能开发

--- app/src/main/java/com/zoombin/greentown/ui/fragment/LevelMessageFragment.kt
+++ app/src/main/java/com/zoombin/greentown/ui/fragment/LevelMessageFragment.kt
@@ -6,10 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import com.zoombin.greentown.R
+import com.zoombin.greentown.model.Message
+import kotlinx.android.synthetic.main.fragment_levelmessage.*
 import kotlinx.android.synthetic.main.layout_titlebar.*
 import me.yokeyword.fragmentation.SupportFragment
 import me.yokeyword.fragmentation.anim.DefaultVerticalAnimator
 import me.yokeyword.fragmentation.anim.FragmentAnimator
+import org.jetbrains.anko.support.v4.toast
 
 /**
  * Created by gejw on 2017/6/9.
@@ -26,6 +29,21 @@ class LevelMessageFragment : BaseBackFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         titleLabel.text = "我要留言"
+
+        sendButton.setOnClickListener {
+            val message = messageEditText.text.toString().trim()
+            if (message == "") {
+                toast("请输入内容")
+                return@setOnClickListener
+            }
+
+            Message.levelMessage(message, {
+                toast("发送成功")
+                pop()
+            }) { message ->
+                if (message != null) toast(message)
+            }
+        }
     }
 
 }
