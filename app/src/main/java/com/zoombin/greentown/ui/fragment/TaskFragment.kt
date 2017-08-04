abc0964d927dc84d7610efcd96f7d4c650d55c3c
Robin <gejw0623@gmail.com>
2017-08-04 21:00:57 +0800

    添加截止

--- app/src/main/java/com/zoombin/greentown/ui/fragment/TaskFragment.kt
+++ app/src/main/java/com/zoombin/greentown/ui/fragment/TaskFragment.kt
@@ -88,7 +88,7 @@ class TaskFragment : BaseBackFragment() {
 
                 nameTextView.text = item.task_name
                 rewardValueTextView.text = "成就点${item.points}，绿币${item.coins}，金币${item.golds}"
-                pickButton.text = if (item.started == 0) "未开始" else if (item.picked == 1) "已领取" else "领取"
+                pickButton.text = if (item.started == 2) "已截止" else if (item.started == 0) "未开始" else if (item.picked == 1) "已领取" else "领取"
                 finishButton.text = if (item.finished == 1) "已完成" else "未完成"
                 finishButton.isEnabled = false
                 pickButton.isEnabled = item.picked == 0 && item.started == 1
