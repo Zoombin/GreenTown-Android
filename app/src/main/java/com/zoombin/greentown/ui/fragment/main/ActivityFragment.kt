764ba6756cc638d33abb3b69024f422b9084c493
Robin <gejw0623@gmail.com>
2017-09-23 23:18:15 +0800

    标题栏添加

--- app/src/main/java/com/zoombin/greentown/ui/fragment/main/ActivityFragment.kt
+++ app/src/main/java/com/zoombin/greentown/ui/fragment/main/ActivityFragment.kt
@@ -1,11 +1,33 @@
 package com.zoombin.greentown.ui.fragment.main
 
+import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.zoombin.greentown.R
+import com.zoombin.greentown.ui.fragment.GiftFragment
+import kotlinx.android.synthetic.main.fragment_gift.*
+import kotlinx.android.synthetic.main.layout_titlebar.*
 import me.yokeyword.fragmentation.SupportFragment
+import org.jetbrains.anko.support.v4.toast
 
 /**
  * Created by gejw on 2017/9/23.
  */
 
 class ActivityFragment : SupportFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_main_activity, null)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        titleLabel.visibility = View.VISIBLE
+        titleLabel.text = "歌林活动"
+    }
 
 }
--- app/src/main/java/com/zoombin/greentown/ui/fragment/main/MemberFragment.kt
+++ app/src/main/java/com/zoombin/greentown/ui/fragment/main/MemberFragment.kt
@@ -1,5 +1,11 @@
 package com.zoombin.greentown.ui.fragment.main
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.zoombin.greentown.R
+import kotlinx.android.synthetic.main.layout_titlebar.*
 import me.yokeyword.fragmentation.SupportFragment
 
 /**
@@ -7,5 +13,16 @@ import me.yokeyword.fragmentation.SupportFragment
  */
 
 class MemberFragment: SupportFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_main_member, null)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        titleLabel.visibility = View.VISIBLE
+        titleLabel.text = "成员信息"
+    }
 
 }
